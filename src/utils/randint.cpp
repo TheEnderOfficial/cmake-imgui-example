5c5d1e37c1fe8ae927c67cf07e8a81edd6352237
TheEnderOfficial <enderofficial.dp@yandex.ru>
2023-02-26 18:39:42 +0300

    truly random numbers

--- src/utils/randint.cpp
+++ src/utils/randint.cpp
@@ -4,9 +4,11 @@
 
 #include <cstdlib>
 #include "utils.h"
+#include "time.h"
 
 namespace AppUtils {
     int randRange(int min, int max) {
+        srand(time(NULL));
         return rand() % (max - min) + min;
     }
 }
